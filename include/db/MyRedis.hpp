170f4fafa7b8c4366852ffcf58c9289bf7b564a0
HanKangKai <HanKangKai@qq.com>
2024-07-29 14:28:34 +0800

    add MyRedis.hpp MyRedis.cpp

---
+++ include/db/MyRedis.hpp
@@ -0,0 +1,26 @@
+#ifndef MYREDIS_HPP
+#define MYREDIS_HPP
+#include <hiredis/hiredis.h>
+#include <iostream>
+#include <string>
+#include "Logger.hpp"
+
+namespace DB {
+    class MyRedis {
+    private:
+        
+
+        
+
+
+    };
+
+}
+
+
+
+
+
+
+
+#endif
